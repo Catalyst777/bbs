f761d0ecf0761e37cd4b5ddc0ebd6f47e2869933
Catalyst777 <takehiro.lijima777@gmail.com>
2021-03-25 21:33:51 +0900

    20210325

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,23 @@
 class PostsController < ActionController::Base
   def index
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
+  end
+  
+  private
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :contents, :date, :name, :update)
+  end
+
 end
