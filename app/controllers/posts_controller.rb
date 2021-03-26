4d8ed628f1618ca2ed46de8978251f87ea668770
Catalyst777 <takehiro.lijima777@gmail.com>
2021-03-26 18:35:48 +0900

    20210326

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,13 +7,30 @@ class PostsController < ActionController::Base
   end
 
   def create
-    Post.create(post_params)
+    post = Post.create(post_params)
+    redirect_to post
   end
   
   private
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+　end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.delete
+    redirect_to posts_path
   end
 
   def post_params
